59cb91d1cace2453c9c8281d431515e2d5eb3c06
Vladyslav Burakov <v.a.burakov@gmail.com>
2017-05-22 21:44:28 +1000

    Continue with Maybe

---
+++ src/main/java/vlad/functional/heaven/higher_order/Holed.java
@@ -0,0 +1,5 @@
+package vlad.functional.heaven.higher_order;
+
+public interface Holed<T extends Holed<T, ?>, A> {
+
+}
--- src/main/java/vlad/functional/heaven/maybe/Maybe.java
+++ src/main/java/vlad/functional/heaven/maybe/Maybe.java
@@ -1,9 +1,11 @@
 package vlad.functional.heaven.maybe;
 
+import vlad.functional.heaven.higher_order.Holed;
+
 import java.util.function.Function;
 import java.util.function.Supplier;
 
-public abstract class Maybe<A> {
+public abstract class Maybe<A> implements Holed<Maybe<?>, A> {
 
     Maybe() {
 
@@ -17,12 +19,28 @@ public abstract class Maybe<A> {
         return Nothing.instance();
     }
 
+    public static <A> Maybe<A> resolve(Holed<Maybe<?>, A> holed) {
+        return (Maybe<A>) holed;
+    }
+
     public abstract <B> B cast(Function<Nothing<A>, B> nothing, Function<Just<A>, B> just);
 
     public final <B> B match(Supplier<B> nothingMatcher, Function<A, B> justMatcher) {
         return cast(
                 nothing -> nothingMatcher.get(),
                 just -> justMatcher.apply(just.value()));
+    }
+
+    public final <B> Maybe<B> map(Function<A, B> f) {
+        return match(
+                () -> nothing(),
+                a -> just(f.apply(a)));
+    }
+
+    public final <B> Maybe<B> flatMap(Function<A, Maybe<B>> f) {
+        return match(
+                () -> nothing(),
+                a -> f.apply(a));
     }
 
     @Override
